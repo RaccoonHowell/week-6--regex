8e5aec92531848dd8cd1742d6938a1d4039d38d6
Rachel <rac777blue@hotmail.com>
2020-02-12 10:14:10 +0000

    amended challenge 5 to split at commas as well as spaces and added comments with alternative regex

--- 05-words.php
+++ 05-words.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 function words (string $str) : array {
 
-    return preg_split("/\s+/", $str);
+    return preg_split("/[, ]+/", $str);
 }
+
+// [^\w]+
+// [,\s]+
 
 var_dump(words("This is a sentence")); // ["This", "is", "a", "sentence"]
 var_dump(words("Hello, my name is Jessica")); // ["Hello", "my", "name", "is", "Jessica"]
