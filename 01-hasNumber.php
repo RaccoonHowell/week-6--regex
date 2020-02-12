410650878a9d79a7e830f2e5d957c601ffd45795
Rachel <rac777blue@hotmail.com>
2020-02-12 09:57:42 +0000

    removed unnecessary + after d

--- 01-hasNumber.php
+++ 01-hasNumber.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 function hasNumber(string $str) : bool {
     
-    return preg_match("/\d+/", $str) === 1;
+    return preg_match("/\d/", $str) === 1;
 }
 
 var_dump(hasNumber("blah")); // bool(false)
